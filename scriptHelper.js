4c0a35f4624fd026f5ad79dd62f15498bd105f86
nikkenw <toucanstubbs@outlook.com>
2021-08-22 17:09:46 -0500

    more minor adjustments send halp

--- scriptHelper.js
+++ scriptHelper.js
@@ -40,16 +40,18 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     validateInput(fuelLevel.value) === "Empty" ||
     validateInput(cargoLevel.value) === "Empty"
   ) {
-    return window.alert("All fields required");
+    window.alert("All fields required");
+    return;
   } else if (
     validateInput(pilot.value) === "Is a Number" ||
     validateInput(copilot.value) === "Is a Number" ||
     validateInput(fuelLevel.value) === "Not a Number" ||
     validateInput(cargoLevel.value) === "Not a Number"
   ) {
-    return window.alert(
+    window.alert(
       "Incorrect input type: Pilot and Copilot should be strings, fuel level and cargo mass should be numbers."
     );
+    return;
   }
 
   let fuelStatus = "Fuel level high enough for launch";
